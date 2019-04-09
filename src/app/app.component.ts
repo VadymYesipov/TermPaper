64acd288c1c302d3bb694ef139d4a6290b6fdaa0
Vadim <v-esipov@mail.ua>
2019-04-09 11:05:59 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {MatPaginator, MatSort, MatTableDataSource} from "@angular/material";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'spring-angular';
+export class AppComponent implements OnInit {
+
+  title = 'Spring Mvc Angular Tutorial';
+
+  @ViewChild(MatSort)
+  sort: MatSort;
+  @ViewChild(MatPaginator)
+  paginator: MatPaginator;
+
+  dataSource = new MatTableDataSource<any>();
+  displayedColumns: string[] = ['id', 'brand name', 'model name', 'price', 'description', 'image'];
+
+  bicycles: any;
+
+  readonly APP_URL = 'http://localhost:8080';
+
+  constructor(private _http: HttpClient) { }
+
+  ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.getAllBicycles();
+  }
+
+  getAllBicycles() {
+    this._http.get(this.APP_URL + '/bicycles').subscribe(
+      data => {
+        this.bicycles = data;
+        this.dataSource.data = this.bicycles;
+        this.dataSource.sort = this.sort;
+      },
+      error => {
+        console.log('Error occured', error);
+      }
+    );
+  }
+
+  getContent(element: any, column: string) {
+    switch (column){
+      case 'brand name':
+        return element['brand']['brandName'];
+      case 'model name':
+        return element['brand']['modelName'];
+      case 'image':
+        return element[column][column];
+      default:
+        return element[column];
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from './material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
